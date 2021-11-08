a7cdf9e90250c5771d5067ef73650a38431a0c1f
Mila Ziabchenko <mi.podgurska@gmail.com>
2021-11-08 12:26:17 +0200

    new file:   css/select.css 	renamed:    style.css -> css/style.css 	modified:   index.html 	modified:   js/main.js 	new file:   js/select.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-import {songs, images} from './constants.js';
+import { songs, images } from './constants.js';
+import Select from './select.js';
 
 const musicContainer = document.querySelector('.music-container');
 const playBtn = document.querySelector('#play');
@@ -9,19 +10,32 @@ const audio = document.querySelector('#audio');
 const cover = document.querySelector('#cover');
 const progress = document.querySelector('.progress');
 const progressContainer = document.querySelector('.progress-container');
+const selectElements = document.querySelectorAll('[data-custom]');
+const selectOptions = document.querySelectorAll('.custom-select-option');
+const options = document.querySelectorAll('#option');
 
 // Keep track of songs and images
 let songIndex = 0;
 let imageIndex = 0;
+let selectIndex = 0;
 
-// Initially load song and image into DOM
+// Initially load song and image into the DOM
 loadSong(songs[songIndex], images[imageIndex]);
+
+// Load song options into the dropdown
+loadSongOptions();
 
 // Update song details
 function loadSong(song, img) {
   title.textContent = song;
   audio.src = `music/${song}.mp3`;
   cover.src = `images/${img}.jpg`;
+}
+
+function loadSongOptions() {
+  options.forEach(option => {
+    option.textContent = songs[selectIndex++];
+  });
 }
 
 function playSong() {
@@ -68,6 +82,15 @@ function nextSong() {
   playSong();
 }
 
+function selectedSong() {
+  songIndex = selectIndex;
+  imageIndex = selectIndex;
+
+  loadSong(songs[songIndex], images[imageIndex]);
+
+  playSong();
+}
+
 function updateProgress(e) {
   const { duration, currentTime } = e.srcElement;
   const progressPercent = (currentTime / duration) * 100;
@@ -98,6 +121,19 @@ prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 audio.addEventListener('ended', nextSong);
 
+// This event listener doesn't hear the 'click' event on select options, and I don't know why... (((
+selectOptions.forEach(option =>
+  option.addEventListener('click', e => {
+    selectedSong;
+    console.log(e.target, 'Clicked');
+  })
+);
+
 // Song progress events
 audio.addEventListener('timeupdate', updateProgress);
 progressContainer.addEventListener('click', setProgress);
+
+// Creating select options
+selectElements.forEach(selectElement => {
+  new Select(selectElement);
+});
